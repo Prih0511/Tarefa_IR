6cdcabcae82a12cfe6cb651f553846ec971967d4
Priscila Neves <kpriscilaneves@gmail.com>
2022-05-16 18:11:06 -0300

    tarefa ir

---
+++ ir.py
@@ -0,0 +1,38 @@
+#boas vindas
+print("Olá, vamos calcular seu imposto de renda?")
+
+#string
+salario = int(input("Digite seu salário:"))
+
+#tentando fase 1
+if salario <= 1903.98:
+    print("Livre de imposto de renda.")
+elif salario >= 1903.99 and salario <= 2826.65:
+    #7.5% do salario
+    porcentagem = 7.5 / 100
+    #conta
+    sobra_salario = salario - porcentagem * salario
+    valor_pagar = porcentagem * salario
+    print("sua aliquota é de 7.5% valor a pagar é de: R$", valor_pagar)
+elif salario >= 2826.66 and salario <= 3751.05:
+    #15% do salario
+    porcentagem = 15 / 100
+    #conta
+    sobra_salario = salario - porcentagem * salario
+    valor_pagar = porcentagem * salario
+    print("sua aliquola é de 15% valor a pagar é de: R$", valor_pagar)
+elif salario >= 3751.06 and salario <= 4664.68:
+    #22.5% do salario
+    porcentagem = 22.5 / 100
+    #conta
+    sobra_salario = salario - porcentagem * salario
+    valor_pagar = porcentagem * salario
+    print("sua aliquola é de 22.5% valor a pagar é de: R$", valor_pagar)
+else:
+    #27.5% do salario
+    porcentagem = 27.5 / 100
+    #conta
+    sobra_salario = salario - porcentagem * salario
+    valor_pagar = porcentagem * salario
+    print("sua aliquola é de 27.5% valor a pagar é de: R$", valor_pagar)
+    #Obrigada.
